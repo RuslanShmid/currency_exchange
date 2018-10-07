753b616ec173fad01f93285b891256afea6d9f27
Ruslan Shmid <ruslan.shmid.rsd@gmail.com>
2018-10-07 20:32:06 +0300

    Change erb to haml
    Create exchange rate subscription new action view
    Update application layout
    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :exchange_rate_subscriptions, only: %i[new create]
 end
