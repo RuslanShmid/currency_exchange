9ac45112f02b4923455037b70a900fe30efc18e1
Ruslan Shmid <ruslan.shmid.rsd@gmail.com>
2018-10-07 20:55:25 +0300

    Add CurrencyExchangeClient service
    Add create action ExchangeRateSuscriptions Controller

--- app/controllers/exchange_rate_subscriptions_controller.rb
+++ app/controllers/exchange_rate_subscriptions_controller.rb
@@ -1,3 +1,26 @@
 class ExchangeRateSubscriptionsController < ApplicationController
   def new; end
+
+  def create
+    response = CurrencyExchangeClient.convert(exchange_rate_params)
+    if response['error'].zero?
+      ex_rate = ExchangeRate.find_or_create_by(exchange_rate_params)
+      if current_user.exchange_rate.exists?(ex_rate.id)
+        flash[:success] = "You already subscribed to: 'from: #{exchange_rate_params['from']}, to: #{exchange_rates_params['to']}'"
+      else
+        current_user.exchange_rate.append(ex_rate)
+        flash[:error] = "You subscribed to: 'from: #{exchange_rate_params['from']}, to: #{exchange_rates_params['to']}'"
+      end
+      redirect_to exchange_rate_subscriptions_path
+    else
+      flash[:error] = "Error: #{response['error_message']}"
+      render :new
+    end
+  end
+
+  private
+
+  def exchange_rate_params
+    params.require(:exchange_rate_subscription).permit(:from, :to)
+  end
 end
---
+++ app/services/currency_exchange_client.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class CurrencyExchangeClient
+
+  CURRENCY_EXCHANGE_URL = 'https://www.amdoren.com/api/currency.php'
+
+  def self.convert(params)
+    JSON.parse(make_request(params))
+  end
+
+  def self.make_request(params)
+    RestClient::Request.execute(
+      method: :get,
+      url: CURRENCY_EXCHANGE_URL,
+      headers: {
+        params: {
+          api_key: ENV['CURRENCY_API_KEY'],
+          from: params['from'],
+          to: params['to'],
+          amount: '50'
+        }
+      }
+    )
+  end
+end
+
